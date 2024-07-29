f8b3b9f8cf6c878c0a4d3f723f729ea52260c52f
sagarsubedi <sagar.subedi@upaya.com.np>
2024-07-29 15:36:28 +0545

    changes

--- frontend/src/VideoPlayer.tsx
+++ frontend/src/VideoPlayer.tsx
@@ -51,7 +51,7 @@ const VideoPlayer = () => {
   //   }
   // };
   return (
-    <div className="flex flex-col  items-center justify-center p-6 bg-gray-100 border-2 border-dashed border-gray-300 rounded-lg">
+    <div className="flex flex-col h-[100vh] items-center justify-center p-6 bg-gray-100 border-2 border-dashed border-gray-300 rounded-lg">
       <div className="flex gap-8">
         <div className="w-[30%]">
           {
@@ -63,14 +63,14 @@ const VideoPlayer = () => {
           }
         </div>
 
-      <div className="video-player w-full">
-      <h2 className="text-xl text-blue-700 font-semibold mb-2"> Now Playing : {setClickedVideoObject?.name ?? ''}</h2>
+      <div className="video-player w-full h-[60vh]">
+      <h2 className="text-xl text-blue-700 font-semibold mb-2"> Now Playing : {setClickedVideoObject?.name == 'bound dispatchSetState' ? 'Nothing playing' : setClickedVideoObject?.name}</h2>
       <ReactPlayer
         ref={playerRef}
         url={clickedVideoObject?.url}
         controls
         width="100%"
-        height="auto"
+        height="80%"
         onProgress={handleProgress}
         // onPause={handlePause}
         // onEnded={handlePause}
